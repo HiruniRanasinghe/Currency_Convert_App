e786c25703473ffbf497bb30a22a261529f1de24
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-12-26 06:38:17 +0000

    import useState and useEffect

--- myapp/App.js
+++ myapp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
